7ebce5d53d0cafa19dcb7dd4ae511f2bec07dfe0
mike <ligopsyxakis.m@gmail.com>
2020-01-02 16:37:55 +0200

    change visibility of cards on start

--- Week2/weight-converter/weight.js
+++ Week2/weight-converter/weight.js
@@ -1,16 +1,15 @@
-for (let i = 0; i < 3; i++){
-    document.getElementsByClassName("card")[i].style.visibility = "hidden";
-}
-
 const grams = document.getElementById("grams");
 const kilograms = document.getElementById("kilogramsOutput");
 const pounds = document.getElementById("poundsOutput");
 const ounces = document.getElementById("ouncesOutput");
 
+grams.onclick = function(){
+    document.getElementsByClassName("card")[0].className = "card bg-primary text-left mb-2 visible";
+    document.getElementsByClassName("card")[1].className = "card bg-success text-left mb-2 visible";
+    document.getElementsByClassName("card")[2].className = "card bg-danger text-left mb-2 visible";
+}
+
 grams.addEventListener("input",function() {
-    for (let i = 0; i < 3; i++){
-        document.getElementsByClassName("card")[i].style.visibility = "visible";
-    }
     kilograms.innerHTML = parseFloat(grams.value) * 0.001;
     pounds.innerHTML = (parseFloat(grams.value) * 0.00220462262).toFixed(4);
     ounces.innerHTML = (parseFloat(grams.value) * 0.0352739619).toFixed(4);
